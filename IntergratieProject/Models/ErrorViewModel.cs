3f4485b286dbdbd2b5bebf7e3a8a77646d931724
Mehdi <mehdi.faouzi@student.kdg.be>
2026-02-23 13:38:40 +0100

    Initial commit

---
+++ IntergratieProject/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace IntergratieProject.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
