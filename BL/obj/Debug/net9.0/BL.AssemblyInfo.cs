6dd6685aa049c54bd25110380106a976e1e46402
Nadia Boudouft Escobar <nadia.boudouftescobar@student.kdg.be>
2026-03-02 12:27:03 +0100

    testen

--- BL/obj/Debug/net9.0/BL.AssemblyInfo.cs
+++ BL/obj/Debug/net9.0/BL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f70433b4b4aebd343f3e2900184061f9b97ffd85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3fc2e688b296d0d513790592b5229ebc59cfca2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
