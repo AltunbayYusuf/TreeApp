f93c10c14e9bf36a0bef1d3c89679df718db61d1
Xenne De Wreede <xenne.dewreede@student.kdg.be>
2026-03-05 16:04:07 +0100

    id's toegevoegd

--- Domain/questions/QuestionList.cs
+++ Domain/questions/QuestionList.cs
@@ -4,6 +4,8 @@ namespace IntergratieProject.Domain.Questions;
 
 public class QuestionList
 {
+    public int Id { get; set; }
+
     public List<Section> Sections { get; set; }
     public Project Project  { get; set; }
 }
