5f39703091fe980cb1087a8be82053dbd7b51bf1
Xenne De Wreede <xenne.dewreede@student.kdg.be>
2026-04-20 10:35:40 +0200

    final fixing about ap

--- UI-MVC/Models/SubAdminDashboardViewModel.cs
+++ UI-MVC/Models/SubAdminDashboardViewModel.cs
@@ -14,13 +14,4 @@ public class SubAdminDashboardViewModel
    
 }
 
-public class ProjectSummaryViewModel
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Status { get; set; }
-    public string Form { get; set; }
-    public int ParticipantsCount { get; set; }
-    public int IdeasCount { get; set; }
-    public DateTime ReleaseDate { get; set; }
-}
+
--- UI-MVC/Program.cs
+++ UI-MVC/Program.cs
@@ -118,7 +118,13 @@ void SeedIdentity(UserManager<ApplicationUser> userManager, RoleManager<Identity
         SubPlatformSlug = "kdg-hogeschool"
     };
     userManager.CreateAsync(kdg, "Test123!").Wait();
-
+    var ap = new ApplicationUser
+    {
+        UserName = "ap@gmail.com",
+        Email = "ap@gmail.com",
+        SubPlatformSlug = "ap-hogeschool"
+    };
+    userManager.CreateAsync(ap, "Test123!").Wait();
     var subAdminRole = new IdentityRole
     {
         Name = CustomIdentityConstants.SubAdminRoleName
@@ -132,7 +138,8 @@ void SeedIdentity(UserManager<ApplicationUser> userManager, RoleManager<Identity
 
 
     userManager.AddToRoleAsync(adminuser, CustomIdentityConstants.GeneralAdminRoleName).Wait();
-   userManager.AddToRoleAsync(kdg, CustomIdentityConstants.SubAdminRoleName).Wait();
+    userManager.AddToRoleAsync(kdg, CustomIdentityConstants.SubAdminRoleName).Wait();
+    userManager.AddToRoleAsync(ap, CustomIdentityConstants.SubAdminRoleName).Wait();
 }
 
 public partial class Program
