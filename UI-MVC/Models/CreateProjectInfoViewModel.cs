4754a965db4d923d48a21f452cb3c09e067806f2
Faizaan Mirza Baig <faizaan.mirzabaig@student.kdg.be>
2026-05-18 10:54:30 +0200

    duurtijd toegevoegd

--- UI-MVC/Models/CreateProjectInfoViewModel.cs
+++ UI-MVC/Models/CreateProjectInfoViewModel.cs
@@ -14,7 +14,9 @@ public class CreateProjectInfoViewModel
     [MinLength(4, ErrorMessage = "Introductietekst moet minstens 4 tekens bevatten.")]
     [MaxLength(1000, ErrorMessage = "Introductietekst mag maximaal 1000 tekens bevatten.")]    
     public string Introduction { get; set; } = "";
-
+    
+    [Range(1, 999, ErrorMessage = "Tijdsduur moet minstens 1 minuut zijn.")]
+    public int Duration { get; set; } = 10;
     public IFormFile? PhotoUpload { get; set; }
 
     public string? PhotoUri { get; set; }
