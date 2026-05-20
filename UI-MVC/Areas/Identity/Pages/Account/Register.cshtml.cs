016cf18f0f6f407fbb6f129be05325f0416cb59a
Nadia Boudouft Escobar <nadia.boudouftescobar@student.kdg.be>
2026-05-20 15:08:24 +0200

    register toegevoegd

--- UI-MVC/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ UI-MVC/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -1,5 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
+
 #nullable disable
 
 using System;
@@ -10,6 +11,9 @@ using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading;
 using System.Threading.Tasks;
+using IntegratieProject.BL.Domain.project;
+using IntegratieProject.BL.Domain.users;
+using IntegratieProject.DAL.Ef;
 using IntegratieProject.DAL.Identity;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
@@ -17,6 +21,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
 
@@ -30,13 +35,16 @@ namespace IntegratieProject.UI.MVC.Areas.Identity.Pages.Account
         private readonly IUserEmailStore<ApplicationUser> _emailStore;
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
+        private readonly TreeDbContext _context;
+        public List<SelectListItem> SubPlatforms { get; set; }
 
         public RegisterModel(
             UserManager<ApplicationUser> userManager,
             IUserStore<ApplicationUser> userStore,
             SignInManager<ApplicationUser> signInManager,
             ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
+            IEmailSender emailSender,
+            TreeDbContext context)
         {
             _userManager = userManager;
             _userStore = userStore;
@@ -44,6 +52,7 @@ namespace IntegratieProject.UI.MVC.Areas.Identity.Pages.Account
             _signInManager = signInManager;
             _logger = logger;
             _emailSender = emailSender;
+            _context = context;
         }
 
         /// <summary>
@@ -63,6 +72,9 @@ namespace IntegratieProject.UI.MVC.Areas.Identity.Pages.Account
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
+        [TempData]
+        public string StatusMessage { get; set; }
+
         public IList<AuthenticationScheme> ExternalLogins { get; set; }
 
         /// <summary>
@@ -71,85 +83,109 @@ namespace IntegratieProject.UI.MVC.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
-            [Required]
-            [EmailAddress]
-            [Display(Name = "Email")]
+            [Required(ErrorMessage = "De naam is verplicht.")]
+            public string Name { get; set; }
+
+            [Required(ErrorMessage = "Het e-mailadres is verplicht.")]
+            [EmailAddress(ErrorMessage = "Vul een geldig e-mailadres in.")]
             public string Email { get; set; }
 
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
-            [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [Required(ErrorMessage = "Het wachtwoord is verplicht.")]
+            [StringLength(100, ErrorMessage = "Het wachtwoord moet minstens {2} karakters lang zijn.",
+                MinimumLength = 6)]
             [DataType(DataType.Password)]
-            [Display(Name = "Password")]
             public string Password { get; set; }
 
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
+            [Required(ErrorMessage = "Je moet het wachtwoord bevestigen.")]
             [DataType(DataType.Password)]
-            [Display(Name = "Confirm password")]
-            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            [Compare("Password", ErrorMessage = "De wachtwoorden komen niet overeen.")]
             public string ConfirmPassword { get; set; }
+
+            [Required(ErrorMessage = "Selecteer een subplatform.")]
+            public int? SubPlatformId { get; set; }
         }
 
 
         public async Task OnGetAsync(string returnUrl = null)
         {
             ReturnUrl = returnUrl;
-            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+
+            ExternalLogins = (await _signInManager
+                .GetExternalAuthenticationSchemesAsync()).ToList();
+
+            SubPlatforms = _context.SubPlatforms
+                .Select(sp => new SelectListItem
+                {
+                    Value = sp.Id.ToString(),
+                    Text = sp.CompanyName
+                }).ToList();
         }
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
             returnUrl ??= Url.Content("~/");
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+
             if (ModelState.IsValid)
             {
                 var user = CreateUser();
 
+                // 1. Haal het subplatform op VOORDAT we de user aanmaken in Identity
+                var subPlatform = _context.SubPlatforms
+                    .FirstOrDefault(sp => sp.Id == Input.SubPlatformId);
+
+                if (subPlatform != null)
+                {
+                    // Belangrijk voor LoginModel: Koppel de slug aan de ApplicationUser!
+                    user.SubPlatformSlug = subPlatform.Slug;
+                }
+
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
                 {
+                    await _userManager.AddToRoleAsync(user, CustomIdentityConstants.SubAdminRoleName);
+
+                    var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    await _userManager.ConfirmEmailAsync(user, token);
+
+                    var generalAdmin = _context.GeneralAdmins.FirstOrDefault();
+                    var subPlatformd = _context.SubPlatforms.FirstOrDefault(sp => sp.Id == Input.SubPlatformId);
+
+                    var subAdmin = new SubAdmin
+                    {
+                        Name = Input.Name,
+                        IdentityUserId = user.Id,
+                        GeneralAdmin = generalAdmin,
+                        SubPlatform = subPlatformd,
+                        Projects = new List<Project>()
+                    };
+
+                    _context.SubAdmins.Add(subAdmin);
+                    await _context.SaveChangesAsync();
+
                     _logger.LogInformation("User created a new account with password.");
 
-                    var userId = await _userManager.GetUserIdAsync(user);
-                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                    var callbackUrl = Url.Page(
-                        "/Account/ConfirmEmail",
-                        pageHandler: null,
-                        values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
-                        protocol: Request.Scheme);
-
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-
-                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
-                    {
-                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
-                    }
-                    else
-                    {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
-                        return LocalRedirect(returnUrl);
-                    }
+                    StatusMessage = $"Account voor {Input.Name} is succesvol aangemaakt en direct geactiveerd!";
+                    return RedirectToPage();
                 }
+
                 foreach (var error in result.Errors)
                 {
                     ModelState.AddModelError(string.Empty, error.Description);
                 }
             }
+
+            // 2. HERLAAD de SubPlatforms lijst als we hier terecht komen (als formulier validatie faalt)
+            SubPlatforms = _context.SubPlatforms
+                .Select(sp => new SelectListItem
+                {
+                    Value = sp.Id.ToString(),
+                    Text = sp.CompanyName
+                }).ToList();
 
             // If we got this far, something failed, redisplay form
             return Page();
@@ -164,8 +200,8 @@ namespace IntegratieProject.UI.MVC.Areas.Identity.Pages.Account
             catch
             {
                 throw new InvalidOperationException($"Can't create an instance of '{nameof(ApplicationUser)}'. " +
-                    $"Ensure that '{nameof(ApplicationUser)}' is not an abstract class and has a parameterless constructor, or alternatively " +
-                    $"override the register page in /Areas/Identity/Pages/Account/Register.cshtml");
+                                                    $"Ensure that '{nameof(ApplicationUser)}' is not an abstract class and has a parameterless constructor, or alternatively " +
+                                                    $"override the register page in /Areas/Identity/Pages/Account/Register.cshtml");
             }
         }
 
@@ -175,6 +211,7 @@ namespace IntegratieProject.UI.MVC.Areas.Identity.Pages.Account
             {
                 throw new NotSupportedException("The default UI requires a user store with email support.");
             }
+
             return (IUserEmailStore<ApplicationUser>)_userStore;
         }
     }
