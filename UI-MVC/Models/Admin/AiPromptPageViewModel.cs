debf3af4d76d1b73022b7d427c2af8305272b576
Xenne De Wreede <xenne.dewreede@student.kdg.be>
2026-05-21 19:32:46 +0200

    prompt aangepast + dubbele label verwijderd

---
+++ UI-MVC/Models/Admin/AiPromptPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace IntegratieProject.UI.MVC.Models.Admin;
+
+public class AiPromptPageViewModel
+{
+    public IList<AiPromptViewModel> Prompts { get; set; } = new List<AiPromptViewModel>();
+
+    public IList<string> TextModels { get; set; } = new List<string>();
+    public IList<string> ImageModels { get; set; } = new List<string>();
+
+    public string SelectedTextModel { get; set; } 
+    public string SelectedImageModel { get; set; } 
+}
